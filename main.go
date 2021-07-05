b950526004e84f451833b184ddea422fd8b62ee5
waleed reda <walidreda428@gmail.com>
2021-07-05 14:14:27 +0200

    deploy

--- main.go
+++ main.go
@@ -20,6 +20,6 @@ func main() {
 	port := os.Getenv("PORT")
 	router := mux.NewRouter()
 	router.HandleFunc("/", functions.Welcome)
-	router.HandleFunc("/verify", functions.Signin).Methods("GET")
+	router.HandleFunc("/verify", functions.Signin).Methods("POST")
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
